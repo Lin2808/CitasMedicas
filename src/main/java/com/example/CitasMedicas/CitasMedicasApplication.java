4c7073348a4822e0d82ec19eaf45b7c7d6e1d6ce
Lin2808 <linjaviers@hotmail.com>
2023-11-21 23:47:16 -0500

    Archivos modificados para generar el war

--- src/main/java/com/example/CitasMedicas/CitasMedicasApplication.java
+++ src/main/java/com/example/CitasMedicas/CitasMedicasApplication.java
@@ -2,9 +2,15 @@ package com.example.CitasMedicas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CitasMedicasApplication {
+public class CitasMedicasApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CitasMedicasApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitasMedicasApplication.class, args);
